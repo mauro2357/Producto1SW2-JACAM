aee9dbe3270c7a4f953d643be10f9df1868b17b0
jach123 <j_alexander12@hotmail.com>
2015-08-14 22:01:04 -0500

    Proyecto, primera subida

--- src/main/java/Negocio/Localizacion/lugar.java
+++ src/main/java/Negocio/Localizacion/lugar.java
@@ -5,7 +5,11 @@ public class lugar {
 	private String descripcion;
 	private String coordenadas;
 	
-	
+	public lugar(String nombre,String descripcion, String coordenadas){
+		this.nombre=nombre;
+		this.descripcion=descripcion;
+		this.coordenadas=coordenadas;
+	}
 	public String getNombre() {
 		return nombre;
 	}
@@ -24,7 +28,11 @@ public class lugar {
 	public void setCoordenadas(String coordenadas) {
 		this.coordenadas = coordenadas;
 	}
-	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return nombre;
+	}
 	
 
 }
